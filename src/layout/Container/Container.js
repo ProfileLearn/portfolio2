da0bf8f500178e5a02be4c669e7809696f53dc0b
ProfileLearn <profilelearn@gmail.com>
2022-10-01 16:02:24 -0300

    reload event4

--- src/layout/Container/Container.js
+++ src/layout/Container/Container.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Cajon from '../../component/Cajon/Cajon';
 import Header from '../Header/Header'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import App from '../../pages/App/App'
 import Contact from '../../pages/Contact/Contact'
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,9 +11,6 @@ import info from '../../api/json/info.json'
 import Experiencia from '../../pages/Experiencia/Experiencia';
 import Potencial from '../../pages/Potencial/Potencial';
 import Contenido from '../../pages/Contenido/Contenido';
-
-
-
 
 
 
@@ -39,7 +36,9 @@ const Container = () => {
     setMobileOpen(!mobileOpen);
   };
 
-  const preventRefresh = (e) => e.preventDefault();
+  const preventRefresh = (e) => {
+    return <Redirect to='/portfolio2' />
+  };
 
   React.useEffect(() => {
     window.addEventListener('onbeforeunload', preventRefresh)
@@ -56,7 +55,7 @@ const Container = () => {
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Switch>
-          
+          red
           <Route path="/portfolio2/experiencia" render={() => <Experiencia info={info.experiencia} />} />
           <Route path="/portfolio2/estudios" render={() => <Estudios info={info.estudios} />} />
           <Route path="/portfolio2/potencial-adquirido" render={() => <Potencial info={info.potencial} />} />
